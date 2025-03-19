d18f235713073ba2f5bdc1dd8a4d693bfdf43487
Rayen Ben <rayen_bn.75@hotmail.fr>
2025-03-19 16:48:53 +0800

    update language

--- resources/views/frontend/cn-services.blade.php
+++ resources/views/frontend/cn-services.blade.php
@@ -24,7 +24,7 @@
             <div class="col-lg-6 mil-relative mil-mb-90">
 
                 <h4 class="mil-up mil-mb-30">{{ __('Air')}} <span class="mil-thin">{{ __('Freight')}}</span></h4>
-                <p class="mil-up mil-mb-30">{{ __('Usually takes between 7-10 days for Express shipping and 10-15 days for Air cargo, coast calculated by Cubic meters weight. Air shipping in generale counts as expensive but fast shipping solution')}}</p>
+                <p class="mil-up mil-mb-30" style="font-size: 20px">{{ __('Usually takes between 7-10 days for Express shipping and 10-15 days for Air cargo, coast calculated by Cubic meters weight. Air shipping in generale counts as expensive but fast shipping solution')}}</p>
                 <!-- <div class="mil-up">
                     <a href="portfolio-3.html" class="mil-link mil-dark mil-arrow-place">
                         <span>View works</span>
@@ -34,15 +34,15 @@
             </div>
             <div class="col-lg-6 mil-relative mil-mb-90">
                 <h4 class="mil-up mil-mb-30">{{ __('Sea')}} <span class="mil-thin">{{ __('Freight')}}</span></h4>
-                <p class="mil-up mil-mb-30">{{ __('Usually takes between 20-45 days depends on the loading and unloading ports, Sea shipping counts as cost effective but slow shipping and cost calculated by CBM Volume.')}}</p>
+                <p class="mil-up mil-mb-30" style="font-size: 20px">{{ __('Usually takes between 20-45 days depends on the loading and unloading ports, Sea shipping counts as cost effective but slow shipping and cost calculated by CBM Volume.')}}</p>
             </div>
             <div class="col-lg-6 mil-relative mil-mb-90">
                 <h4 class="mil-up mil-mb-30">{{ __('Product')}} <span class="mil-thin">{{ __('Sourcing')}}</span></h4>
-                <p class="mil-up mil-mb-30">{{ __('Our Team of experts in china will help you find, verifiy and negotiate the best price from the verified suppliers. this services cost per product')}}</p>
+                <p class="mil-up mil-mb-30" style="font-size: 20px">{{ __('Our Team of experts in china will help you find, verifiy and negotiate the best price from the verified suppliers. this services cost per product')}}</p>
             </div>
             <div class="col-lg-6 mil-relative mil-mb-90" id="warehouse">
                 <h4 class="mil-up mil-mb-30">{{ __('Warehouse')}} <span class="mil-thin">{{ __('Storage')}}</span></h4>
-                <p class="mil-up mil-mb-30">{{ __('We offer to all our clients a free 15 days storage space, after 15 days client will be charged a small fee for warehouse space depends on the CBM volume of the goods.')}}</p>
+                <p class="mil-up mil-mb-30" style="font-size: 20px">{{ __('We offer to all our clients a free 15 days storage space, after 15 days client will be charged a small fee for warehouse space depends on the CBM volume of the goods.')}}</p>
             </div>
         </div>
     </div>
